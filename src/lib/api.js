06a3607aa325a6188d172f3b6bafcfce3949f371
seltan-g8 <snbda3010@gmail.com>
2025-08-01 17:29:13 +0100

    gg

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://nightalk-backend.vercel.app/';
+const API_BASE_URL = 'link to your backend API';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
